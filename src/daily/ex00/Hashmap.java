cd6da23168a2a208e747057349b1682c4c160e52
220SRS <jesikrjy@gmail.com>
2023-01-16 09:52:52 +0900

    거듭제곱

--- src/daily/ex00/Hashmap.java
+++ src/daily/ex00/Hashmap.java
@@ -1,15 +1,16 @@
+package daily.ex00;
+
 import java.util.HashMap;
 
-public class Main {
+class Hashmap {
     public static void main(String[] args) {
         String[] test = new String[]{"a", "b", "c", "d"};
 
-        HashMap<String, String> result = new HashMap();
+        HashMap<String, String> result = new HashMap<>();
 
         for (int i = 0; i < test.length / 2; i++) {
             result.put(test[i], test[test.length - i - 1]);
         }
-
         System.out.println(result);
     }
 }
