c143490bae2fe9f23405d36fb7b6368fc47ed152
220SRS <jesikrjy@gmail.com>
2023-01-31 10:15:53 +0900

    길이의 최솟값과 최댓값에 해당하는 단어를 삭제하라 만약 같은게 있다면 뒤에 있는 단어를 삭제

---
+++ src/daily/ex15/Extremes.java
@@ -0,0 +1,33 @@
+package daily.ex15;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Extremes {
+    public static void main(String[] args) {
+        String[] output = removeExtremes(new String[]{"a", "b", "c", "def"});
+        System.out.println(Arrays.toString(output)); // --> ["a"', "b"]
+    }
+    public static String[] removeExtremes(String[] arr) {
+
+        if(arr.length == 0) return null;
+
+        String min = arr[0];
+        String max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if(min.length() >= arr[i].length()) {
+                min = arr[i];
+            }
+            if(max.length() <= arr[i].length()) {
+                max = arr[i];
+            }
+        }
+        ArrayList<String> listArr = new ArrayList<>(Arrays.asList(arr));
+
+        listArr.remove(min);
+        listArr.remove(max);
+
+        return listArr.toArray(new String[listArr.size()]);
+    }
+}
