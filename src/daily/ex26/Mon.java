9c68468fbc134345d57917ede0bb5ff6be5bfa41
220SRS <jesikrjy@gmail.com>
2023-02-11 15:08:12 +0900

    프로그래머스 포켓몬 문제

---
+++ src/daily/ex26/Mon.java
@@ -0,0 +1,22 @@
+package daily.ex26;
+
+import java.util.Arrays;
+
+public class Mon {
+    public static void main(String[] args) {
+        int[] test = new int[]{3,1,2,3};
+        System.out.println(solution(test));
+
+    }
+
+    public static int solution(int[] nums) {
+        int choiceMon = nums.length/2;
+
+        nums = Arrays.stream(nums).distinct().toArray();
+
+        if(nums.length < choiceMon) return nums.length;
+
+
+        return choiceMon;
+    }
+}
